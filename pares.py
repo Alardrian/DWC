87106c7fcdfbd3ecfb35fe3300f404fad00ca79f
Adrian_Guinot <aguinotalonso@iessonferrer.net>
2022-09-30 20:31:56 +0200

    Titulo con borde

---
+++ pares.py
@@ -0,0 +1,13 @@
+def contador_pares_1(y):
+    x = 1
+    contador_pares = 1
+    while contador_pares <= y:
+        if (x%2 == 0):
+            print(x)
+            contador_pares += 1
+        x += 1
+
+        
+
+contador_pares_1(100)
+
