ff5e17490769798826b7de334f08d8a9c524b1b4
Adrian_Guinot <aguinotalonso@iessonferrer.net>
2022-10-03 17:53:40 +0200

    math randoms

--- DWC/buscaminas/js/buscaminas.js
+++ DWC/buscaminas/js/buscaminas.js
@@ -1,10 +1,35 @@
 
-document.write("<table>");
-document.write("<tr>");
+let maxFilas = prompt("¿Cuantas filas quieres?");
+let maxColumnas = prompt("¿Cuantas columnas quieres?");
+let numMinas = prompt("¿Cuantas minas quieres introducir?")
 
-for (let i = 0; i < 5; i++) {
-    document.write("<td></td>");
+
+//creamos tablero en html
+document.write("<table>");
+
+
+
+for (let i = 0; i < maxFilas; i++) {
+
+    document.write("<tr>");
+
+    for (let j = 0; j < maxColumnas; j++) {
+        document.write("<td></td>");
+    }
+    
+    document.write("</tr>");
 }
 
-document.write("</tr>");
 document.write("</table>");
+
+//crear array bidimensional para guardar las minas
+
+let arrayTablero = [];
+
+for (let mina = 0; mina < numMinas; mina++){
+    posFila = Math.floor(Math.random()*maxFilas);
+    console.log(posFila);
+  //  arrayTablero[posFila][posColumna] = "MINA";
+}
+
+console.log(arrayTablero);
