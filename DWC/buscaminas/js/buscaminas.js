f9fc634e86e69c806f0fd4d24e39ba91d8ec49eb
Adrian_Guinot <aguinotalonso@iessonferrer.net>
2022-10-03 17:14:54 +0200

    buscaminas.js

---
+++ DWC/buscaminas/js/buscaminas.js
@@ -0,0 +1,10 @@
+
+document.write("<table>");
+document.write("<tr>");
+document.write("<td>mina</td>");
+document.write("<td>1</td>");
+document.write("<td>0</td> ");
+document.write("<td>1</td>");
+document.write("<td>mina</td>");
+document.write("</tr>");
+document.write("</table>");
