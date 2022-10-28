b65e00ccfa9842ae92a09f607fceecddb0ea14ff
Adrian_Guinot <aguinotalonso@iessonferrer.net>
2022-10-28 19:14:14 +0200

    cuadradoMagico copia

--- DWS/cuadradoMagico/cuadrado_magico.php
+++ DWS/cuadradoMagico/cuadrado_magico.php
@@ -1,7 +1,73 @@
 <?php
     class cuadrado{
-        
+        public $suma1Fila;
+        public $filasDistintas = [];
+        public $columnasDistintas = [];
+        public $diagonal1;
+        public $diagonal2;
+
+        function analizarCuadradoMagico($array){
+            $this->suma1Fila = $this->sumaPrimeraFila($array);
+        }
+
+        function sumaPrimeraFila($array){
+           $resultado = array_sum($array[0]);
+           return $resultado;
+        }
+        function sumarFilas($array){
+            $resultado = [];
+            for ($i=0; $i < count($array); $i++) { 
+                $resultado[$i] = array_sum($array[$i]);
+            }
+            return $resultado;
+        }
+
+        function compararFilas($array){
+            $filasDistintas = [];
+            for ($i=0; $i < count($array); $i++) { 
+                
+            }
+            return $resultado;
+        }
+
+        function sumarColumnas($array){
+            $resultado = [];
+            for ($i=0; $i < count($array); $i++) { 
+                for ($j=0; $j < count($array[$i]); $j++) { 
+                    $resultado[$i] += $array[$j][$i];
+                }
+            }
+            return $resultado;
+        }
+
+        function sumarDiagonal1($array){
+            $resultado = 0;
+            $indiceColumna = count($array)-1;
+            for ($i=0; $i < count($array) ; $i++) { 
+                $resultado += $array[$indiceColumna-$i][$i];
+            }
+            return $resultado;
+        }
+
+        function sumarDiagonal2($array){
+            $resultado = 0;
+            for ($i=0; $i < count($array) ; $i++) { 
+                $resultado += $array[$i][$i];
+            }
+            return $resultado;
+
+        }
+        function pintarCuadradoMagico($array){
+            
+            echo '<table>';
+                for ($i=0; $i < count($array) ; $i++) { 
+                    echo '<tr>';
+                    for ($j=0; $j < count($array) ; $j++) { 
+                        echo '<td>'.$array[$i][$j];
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                }
+            echo'</table>';
+        }
     }
-
-
-?>
--- DWS/cuadradoMagico/cuadrado_magico_user.php
+++ DWS/cuadradoMagico/cuadrado_magico_user.php
@@ -5,21 +5,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cuadrado magico</title>
-    <link rel="stylesheet" href="estilos_cuadrado_magico.css">
+    <style>
+    table, td, tr{
+        border: 1px solid rgb(0, 0, 0);
+        border-collapse: collapse;
+        text-align: center;
+    }
+    table{
+        width: 300px;
+        height: 300px;
+    }
+    .contenedor{
+        height: 100%;
+        width: 100%;
+        margin: auto;
+        margin-top: 25vh;
+    }
+    </style>
 </head>
 <body>
+    <div id="contenedor">
+    
 
-    <table>
-        <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-        </tr>   
-    </table>
+    </div>
+    
     <?php
-
+        
         require 'cuadrado_magico.php';
         $cuadrado = new cuadrado();
+
+        $arrays = [[1,1,1],
+                   [2,2,2],
+                   [3,3,3]];
+
+        echo $cuadrado->sumaPrimeraFila($arrays);
+        echo '<br>';
+        echo $cuadrado->analizarCuadradoMagico($arrays);
+        echo '<br>';
+        print_r($cuadrado->sumarFilas($arrays));
+        echo '<br>';
+        print_r($cuadrado->sumarColumnas($arrays));
+        echo '<br>';
+        echo $cuadrado->sumarDiagonal1($arrays);
+        echo '<br>';
+        echo $cuadrado->sumarDiagonal2($arrays);
+        echo '<br>';
+        echo $cuadrado->pintarCuadradoMagico($arrays);
+        echo '<br>';
+        echo $cuadrado->suma1Fila;
+
+    // $objeto = $analizarCuadradoMagico($array)
+    // pintarCuadradoMagico($array,$objeto)
 
     ?>
 </body>
